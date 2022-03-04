7641ffdaa38ee1f80e01acd4fa1f8773ad72a019
Jacob Robinson <robinson.jacob.e@gmail.com>
2022-03-04 00:30:55 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+/** @type {import('gatsby').GatsbyConfig} */
+module.exports = {
+  siteMetadata: {
+      title: ``,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "II4QFfX-_K3bFfHPhYsyNa-oco2btBbqh1V-Aq7R1WE",
+      "spaceId": "5iboz32qd5jw"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
